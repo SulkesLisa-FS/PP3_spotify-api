3754cceac2c55033fed93981418b6e63183eab17
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-05 14:01:36 -0400

    Refactored Code: Made code more modular, Checked spelling, and formatted with Prettier

---
+++ server.js
@@ -0,0 +1,20 @@
+// Importing the express module
+const app = require("./app");
+
+// Importing the environment variable configuration
+require("dotenv").config();
+
+// Import the database
+const connectDB = require("./app/db/config");
+
+// Run the database (MongoDB) asynchronous function
+connectDB();
+
+//  Port Variable from .env file
+const PORT = process.env.PORT || 3000;
+
+// Starting the server and listening on the specified port
+// listening for incoming requests
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT} `);
+});
