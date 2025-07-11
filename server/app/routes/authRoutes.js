590b9240be5a1fc048de707732259251f36382bc
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-11 09:06:35 -0400

    New: authController directory with sub controllers and index

---
+++ server/app/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+
+// Routes / Controllers for Authentication
+router.get("/login")
+router.get("/callback")
+router.get("/logout")
+
