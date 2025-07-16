622c985f3d7181e7c4a91b4303ed111c3afd4aa8
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-16 09:16:35 -0400

    New spotifyRoutes.js // Import spotifyRoutes into Routes index

---
+++ server/app/routes/spotifyRoutes.js
@@ -0,0 +1,10 @@
+// Inport Express
+const express = require("express");
+const router = express.Router();
+
+// Will Add Search Controller Here
+
+// The search endpoint - query parameters are passed via URL
+router.get("/search");
+
+module.exports = router;
