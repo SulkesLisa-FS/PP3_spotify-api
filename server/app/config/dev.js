7630ed7c83c3d5e89ccef045a278f4582aaf5c9c
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-19 17:54:15 -0400

    Changed environment variable names for consistency // updated .env.dist file and redirectUtils

--- server/app/config/dev.js
+++ server/app/config/dev.js
@@ -1,8 +1,8 @@
 module.exports = {
   // Development configuration settings
-  prodURL: process.env.BASE_URL || "http://localhost:3000/api/v1/",
-  mongoURI: process.env.MONGODB_URI || "mongodb://localhost:27017/spotify-app",
-  spotifyClientId: process.env.CLIENT_ID,
-  spotifyClientSecret: process.env.CLIENT_SECRET,
-  spotifyRedirect: process.env.REDIRECT_URI,
+  baseURL: process.env.BASE_URL,
+  mongoURI: process.env.MONGODB_URI,
+  spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  spotifyRedirect: process.env.SPOTIFY_REDIRECT,
 };
