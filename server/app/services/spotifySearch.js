c27953fa40450f675adf3bdc3915baffe30b3a33
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-19 13:13:57 -0400

    New Search service file

---
+++ server/app/services/spotifySearch.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+
+
+// Create a new instance of search
+class SpotifySearch { 
+    // Search: artist, albms and tracks using Spotify Web API
+
+   
+
+// Build Search
+
+
+
+
+// Define and Format Search For Easier UI
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+module.exports = new SpotifySearch();
