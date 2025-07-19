9dc082f0ad95f471dad3765cfde6775b9a575a30
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-19 14:04:31 -0400

    Add search prams and GET request

--- server/app/services/spotifySearch.js
+++ server/app/services/spotifySearch.js
@@ -1,13 +1,45 @@
 const axios = require("axios");
 
 
-// Create a new instance of search
+// Class to handle search request and formats the results for the client UI
 class SpotifySearch { 
     // Search: artist, albms and tracks using Spotify Web API
 
    
 
-// Build Search
+// Build The Search from the search controller prams
+async seachItems(searchPrams ) {
+
+    // Spotify Base Search Endpoint
+    const baseUrl = "https://api.spotify.com/v1/search";
+    // Create search object with appending search parameters
+    const searchUrl = new URL(baseUrl);
+
+    // Search controller search parameters
+    // Query trimmed to avoid leading and trailing spaces
+    searchUrl.searchParams.append("q", searchParams.query.trim());
+    // Default type to artist, albums, and tracks
+      searchUrl.searchParams.append("type", searchParams.type || "artist,album,track");
+    //   limit range
+      searchUrl.searchParams.append("limit", searchParams.limit.toString());
+    //   offset for pagination
+      searchUrl.searchParams.append("offset", searchParams.offset.toString());
+
+      // Check if there is external audio content
+      if (searchPrams.include_external) {
+        searchUrl.searchParams.append("include_external", 
+        searchPrams.include_external);
+        
+      }
+
+
+    // GET - request to get Spotify API data
+    const response = await axios.get(searchUrl.toString(), {
+        // Pass the user token to tell spotify who is makeing the request
+        headers: { Authorization: `Bearer ${accessToken}`},
+    });
+
+}
 
 
 
