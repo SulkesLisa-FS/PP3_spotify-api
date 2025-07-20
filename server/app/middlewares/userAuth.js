3d87e0f5accc9f2227ef9de16fcb253a8e0a8076
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-20 08:36:10 -0400

    Set up userAuth

--- server/app/middlewares/userAuth.js
+++ server/app/middlewares/userAuth.js
@@ -1,7 +1,40 @@
+// Import axios and querysting
+const axios = require("axios");
+const querystring = require("querystring");
+
+
+// Import user model and service file
+const User = require("../models/User");
+const envConfig = require("../config/envConfig");
+const spotifyService = require("../services/spotifyService");
 
 
 
-  const getRefreshToken = async () => {
+// Get the Spotify user ID from the auth header 
+
+
+// Find user in the database
+
+
+
+//  Call service file to check if token is expired true or flase
+
+
+// Refresh token if true
+
+
+// Update user for valid access
+
+
+
+
+
+
+
+  const userAuth = async () => {
+
+//  SPOTIFY'S EXAMPLE: 
+
 
    // Spotify Example
    const refreshToken = localStorage.getItem('refresh_token');
@@ -26,3 +59,5 @@
       localStorage.setItem('refresh_token', response.refresh_token);
     }
   }
+
+  //   module.exports = userAuth;
