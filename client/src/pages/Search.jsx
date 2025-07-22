76c69eb9b1532421d5ef6884ca0d6d409a5d5873
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-22 09:47:56 -0400

    Add todo's to search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,7 +1,23 @@
 import "../App.css";
 import { Box, Text } from "@chakra-ui/react";
 
+// TODO:  Add Background Image backgroundVT1000
+// TODO:  Add Variables currentUser, loading, error
+
+// Notes: -  No Results until user searches. 
+//        -  Change state from title and CTA to Results 
+//           When user begins to type in the search bar
+
+// Spotify Search Page _ After User Authentication
 function Search() {
+
+
+// TODO:  Hook useEffet
+
+// TODO:  Add API function
+
+// TODO:  3 row grid layout
+
   return (
     <Box
       as="div"
@@ -12,13 +28,27 @@ function Search() {
       textAlign="center"
       py={8}
     >
+      {/* TODO:  Import Search Component */}
+      
       <Text as="h2" color="#112236" fontWeight="600" fontSize="3rem" mb={4}>
         Search Page
       </Text>
-      
+        {/* TODO:  H1 Page Title */}
+
+        {/* TODO:  H2 CTA */}
       <Text color="#666" fontSize="lg">
         Search to get started
       </Text>
+
+      {/* TODO:  Grid row 1. Title - Artist */}
+      {/* TODO:  Grid row 1. Cards */}
+
+      {/* TODO:  Grid row 2. Title - Albums */}
+      {/* TODO:  Grid row 2. Cards */}
+
+      {/* TODO:  Grid row 3. Title - Songs */}
+      {/* TODO:  Grid row 3. Cards */}
+
     </Box>
   );
 }
