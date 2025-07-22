c832d8814d8663ba8129a84f52f888b127ab3bd8
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-22 09:59:40 -0400

    Add Todo's for contact us page

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -1,7 +1,12 @@
 import "../App.css";
 import { Box, Text } from "@chakra-ui/react";
 
+// TODO:  Add Background Image backgroundVT1000
+// TODO:  Add Variables
+
 function ContactUs() {
+
+
   return (
     <Box
       as="div"
@@ -12,13 +17,33 @@ function ContactUs() {
       textAlign="center"
       py={8}
     >
+       {/* H1 Page Title */}
       <Text as="h2" color="#112236" fontWeight="600" fontSize="3rem" mb={4}>
-        Search Page
+        Contact Us
       </Text>
       
-      <Text color="#666" fontSize="lg">
-        Search to get started
-      </Text>
+      {/* TODO:  Form Container 
+                [] Solid Black Background
+                [] 2 sections */}
+
+      {/* TODO:  Section 1 - h3 - Title Address:  */}
+      {/* TODO:  Section 1 - p - Physical Address */}
+      {/* TODO:  Section 2 - h3 - Title Send Message:*/}
+      {/* TODO:  Section 2 - Form 
+                            [] Full Name
+                            [] Email
+                            [] Message 
+                            [] Send Button*/}
+
+
+
+
+
+
+
+
+
+
     </Box>
   );
 }
