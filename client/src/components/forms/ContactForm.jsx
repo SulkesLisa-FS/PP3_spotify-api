c46992cc94b3a4d7b6a33b60667d93cbc49b2e44
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-07-30 20:59:44 -0400

    update form

--- client/src/components/forms/ContactForm.jsx
+++ client/src/components/forms/ContactForm.jsx
@@ -60,7 +60,7 @@ function ContactForm() {
         `${values.message}\n\nFrom: ${values.name} (${values.email})`
       );
       // Redirect to mailto link with subject and body
-      window.location.href = `mailto:mpattlee@gmail.com?subject=${subject}&body=${body}`;
+      window.location.href = `mailto:fakemail@gmail.com?subject=${subject}&body=${body}`;
     }
   };
 
